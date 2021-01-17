45bc41c1143751e528f077a5258c4ebe0da67234
Tosha Shigenina <shigenina@gmail.com>
2021-01-18 01:15:39 +0500

    Компонент карты

---
+++ src/components/AddressMap.vue
@@ -0,0 +1,39 @@
+<template>
+  <div ref="googleMap" id="map"></div>
+</template>
+
+<script>
+export default {
+  name: 'AddressMap',
+  props: {
+    coords: Object
+  },
+  data () {
+    return {
+      mapOptions: {
+        zoom: 4,
+        center: this.coords
+      }
+    }
+  },
+  methods: {
+    initMap () {
+      const map = new google.maps.Map(this.$refs.googleMap, this.mapOptions)
+      const marker = new google.maps.Marker({
+        position: this.coords,
+        map: map
+      })
+    }
+  },
+  mounted () {
+    this.initMap()
+  }
+}
+
+</script>
+
+<style scoped lang="sass">
+#map
+  width: 100%
+  height: 400px
+</style>
