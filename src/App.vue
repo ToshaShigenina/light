11100ee759095a9202d34c216bd5381f772eb7b0
Tosha Shigenina <shigenina@gmail.com>
2021-01-18 01:17:20 +0500

    Добавлены стили проекта, обновлен основной компонент

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,92 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Address></Address>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Address from './components/Address.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Address
   }
 }
+
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+$font: 'Arial', sans-serif
+$color-main-bg: #2a2c2d
+$color-address-bg: #3a3d3e
+$color-prop-bg: #282c2d
+$color-light-text: #969c9f
+$color-prop-active-bg: #404749
+$font-size: 16px
+
+*
+  margin: 0
+  padding: 0
+  box-sizing: border-box
+
+html,
+body
+  width: 100%
+  min-height: 100vh
+  height: auto
+
+body
+  background-color: $color-main-bg
+  color: $color-light-text
+  line-height: 1
+  font-size: $font-size
+  font-family: $font
+
+.wrapper
+  display: flex
+  flex-direction: column
+  width: 100%
+  max-width: 1024px
+  margin: 0 auto
+  padding: 14px 8px
+  &__item
+    width: 100%
+    &:last-child
+      margin-top: 14px
+
+.point
+  list-style-type: none
+  &__item
+    margin-top: 10px
+    &:first-child
+      margin-top: 0
+  &__label
+    display: block
+    padding: 10px 14px
+    background-color: $color-address-bg
+  &__radio
+    display: none
+    &:checked + .point__label
+      transition: background-color .5s
+      background-color: $color-prop-active-bg
+
+.prop
+  list-style-type: none
+  display: flex
+  margin-top: 14px
+  &__item
+    margin-right: 12px
+    padding: 8px 12px
+    font-size: 70%
+    background-color: $color-prop-bg
+
+@media (screen and min-width: 768px)
+  .wrapper
+    flex-direction: row
+    &__item
+      flex-basis: 50%
+      &:last-child
+        margin-top: 0
+        margin-left: 14px
 </style>
